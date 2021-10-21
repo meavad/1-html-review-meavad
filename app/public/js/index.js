ff3e7c862444f9a5d8992a4f19adc76f3358ad93
meavad <88206180+meavad@users.noreply.github.com>
2021-10-20 20:12:29 -0400

    Book form

--- app/public/js/index.js
+++ app/public/js/index.js
@@ -1,36 +1,73 @@
-const Someapp = {
-    data() {
-      return {
-        "person": {},
-        }
-    },
-    computed: {
-      prettyBirthday() {
-        return dayjs(this.person.dob.date)
-        .format('D MMM YYYY')
-      }
-    },
-    methods: {
-      fetchUserData()
-        {
-          console.log("A") 
-          fetch('https://randomuser.me/api/') 
-          .then( response => response.json() )    
-          .then( (responseJson) => {              
-               // console.log(json);
-               this.person = responseJson.results[0];
-               console.log("C"); 
-        } )
-          .catch ( (err) => {
-          console.error( err );
-          });
-          console.log("B");
-          }
+const SomeApp = {
+  data() {
+    return {
+      person: {},
+      list: [5,6,7,8],
+      message: "Waiting ...",
+    }
   },
-      
-    created () {   
-      this.fetchUserData();                                
+  computed: {
+      prettyBirthday() {
+          return dayjs(this.person.dob.date)
+          .format('D MMM YYYY')
+      }
+  },
+  methods: {
+      fetchUserData() {
+          //Method 1:
+          fetch('https://randomuser.me/api/')
+          .then(response => response.json())
+          .then((json) => {
+              console.log("Got json back:", json);
+              this.person = json.results[0];
+              console.log("C");
+          })
+          .catch( (error) => {
+              console.error(error);
+          });
+      },
+      fetchBookData() {
+          console.log("Fetching book data for ", s);
+          fetch('/api/books/')
+          .then( response => response.json() )
+          .then( (responseJson) => {
+              console.log(responseJson);
+              this.books = responseJson;
+          })
+          .catch( (err) => {
+              console.error(err);
+          })
+      }
+  },
+  created() {
+      this.fetchUserData();
+      // this.fetchBookData();
   }
 }
-Vue.createApp(Someapp).mount('#randomuser');
-console.log("Z");
+Vue.createApp(SomeApp).mount('#someApp');
+
+// fetch('api/offer/create.php' , {
+//     method: 'POST',
+//     body: JSON.stringify(this.offerForm),
+//     headers: {
+//         "Content-Type": "application/json; charset=utf-8"
+//     }
+// })
+// .then( response => response.json() )
+// .then( json => {
+//    console.log("Returned from post:", json);
+//    this.offers = json;
+//    this.offerForm={};
+// })
+
+selectOffertoEdit(o) {
+    this.selectedOffer = o;
+    this.offerForm = this.selectedOffer;
+},
+resetOfferForm() {
+    this.selectedOffer = null;
+    this.offerForm = {};
+}
+postEditOffer(evt) {
+    this.student
+}
